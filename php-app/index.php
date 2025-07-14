2e90a1c0986b745d1531d92260e9e1b94787981e
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-07-14 18:01:00 +0530

    addint the files

---
+++ php-app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to PHP App</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1><?php echo "Hello, World! 🌍"; ?></h1>
+    <p>This is a simple PHP application served via Nginx + PHP-FPM.</p>
+</body>
+</html>
