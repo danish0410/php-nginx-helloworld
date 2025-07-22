8acc0fd88c3a91f7aaf4d877efe8c88a2a11f96c
danish0410 <thanigai_velan@yahoo.com>
2025-07-22 11:05:28 +0530

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -7,5 +7,6 @@
 <body>
     <h1><?php echo "Hi Raj"; ?></h1>
     <p>I have created simple PHP application for you, pl check and provide yr feedback.</p>
+    <img src="https://nginx-test-bucket-thanigai2808.s3.ap-south-1.amazonaws.com/sample.jpg" alt="S3 Image" width="400">
 </body>
 </html>
