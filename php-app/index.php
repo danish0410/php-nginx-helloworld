00fa8fccb4d817633f9fe7cb473d0c106630cc27
danish0410 <thanigai_velan@yahoo.com>
2025-07-22 12:01:10 +0530

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -7,6 +7,8 @@
 <body>
     <h1><?php echo "Hi Raj"; ?></h1>
     <p>I have created simple PHP application for you, pl check and provide yr feedback.</p>
-    <img src="https://nginx-test-bucket-thanigai2808.s3.ap-south-1.amazonaws.com/sample.jpg" alt="S3 Image" width="400">
+    <img src="https://nginx-test-bucket-thanigai2808.s3.ap-south-1.amazonaws.com/WhatsApp%20Image%202024-11-10%20at%2011.13.50_a866596f.jpg" 
+       alt="S3 Image" 
+       style="max-width:500px; height:auto;">
 </body>
 </html>
